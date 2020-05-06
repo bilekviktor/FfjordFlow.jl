146546fd00e3b620ac95c52f42b39e5ca0e454d9
Viktor <viktor.bilek97@gmail.com>
2020-05-06 14:29:51 +0200

    managing ffjorflow.jl

--- src/FfjordFlow.jl
+++ src/FfjordFlow.jl
@@ -1,7 +1,10 @@
 module FfjordFlow
 
+include("Cnf.jl")
 include("Ffjord.jl")
-include("ResidualFlows.jl")
+include("iResNet.jl")
+include("ResidualFlow.jl")
+include("SpectralNormalization.jl")
 
 export Ffjord
 end
