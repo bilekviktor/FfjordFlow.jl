92aa658bd18df3d8a774f0904ca8be71fd289fa9
Viktor <viktor.bilek97@gmail.com>
2020-07-24 11:57:26 +0200

    minor example changes

--- examples/test.jl
+++ examples/test.jl
@@ -34,8 +34,10 @@ res = reshape(logpdf(mm, xx), 41, 41)
 heatmap(exp.(res))
 #----------------------------ResidualFlow---------------------------------
 #generating dataset
-x = ToyProblems.sixgaussians(150)
-y = RandomBatches(x, 100, 300)
+include("../src/iResNet.jl")
+include("../src/ResidualFlow.jl")
+x = ToyProblems.sixgaussians(20)
+#y = RandomBatches(x, 100, 300)
 
 #ResidualFlow model - first argument requires chain of residual blocks - 1 res block here
 n = 20
@@ -50,12 +52,15 @@ function loss_resnet(x)
 end
 loss_resnet(x)
 
+
 ps = Flux.params(m)
 _data = Iterators.repeated((), 100)
 
 opt = ADAM(0.1)
 sopt = SpecNormalization(0.1) #optimeser for spectral normalization
-specttrain!(x -> loss_resnet(getobs(x)), ps, y, opt, sopt, 1)
+oopt = Flux.Optimise.Optimiser(opt, sopt)
+#specttrain!(() -> loss_resnet(x), ps, _data, opt, sopt, 1)
+Flux.Optimise.train!(() -> loss_resnet(x), ps, _data, oopt)
 
 #converting ResidualFlow to exact iResNet model
 mm = iResNet(m, 5)
